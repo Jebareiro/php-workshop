1760f9b4c4e46da1978e7b8fda1eb08ac18e6743
Jessica Bareiro <jessibareiroaponte@gmail.com>
2025-06-17 20:02:03 -0300

    Uso basico de echo y print

---
+++ introduction/03_echo_print_desafio.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * DESAFIO: echo y print
+ * ----------------------
+ * 1. Crea una variable $nombre con tu nombre.
+ * 2. Usa echo para mostrar: 'Bienvenido [nombre] al sistema de talleres'
+ * 3. Usa print para mostrar el texto HTML: <p>Taller de PHP basico<p>
+ * 4. Extra: Concadena dos string con echo usando comillas simples
+*/
+
+$nombre = "Lujan";
+echo "Bienvenido $nombre al sistema de talleres";
+print "<p>Taller de PHP basico</p>";
+
+echo 'El nombre del alumno es: ' . $nombre . ', Bienvenido';
+
